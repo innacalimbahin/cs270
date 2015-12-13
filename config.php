e63af4ae0d978b454be7f27fb1f28590c20a91f6
innacalimbahin <innacalimbahin@yahoo.com>
2015-12-13 23:23:29 +0800

    Delete config.php

--- config.php
+++
@@ -1,9 +0,0 @@
-<?php
-    $hostname='localhost:3036';
-    $username='root';
-    $password='@gAt0nton';
-    $dbname='cs270';
-
-    mysql_connect($hostname, $username, $password) OR DIE('Unable to connect to database! Please try again later.');
-    mysql_select_db($dbname);
-?>
